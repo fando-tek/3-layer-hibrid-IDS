9818d1426c32b3da6a376faf43b3f877570d7d68
fando-tek <81504312+fando-tek@users.noreply.github.com>
2023-08-15 18:57:52 +0700

    Update model.py

--- IDS dengan 3 lapisan pembelajaran hibrid/model.py
+++ IDS dengan 3 lapisan pembelajaran hibrid/model.py
@@ -8,27 +8,21 @@ import time
 import joblib
 import warnings
 from tensorflow import keras
-#warnings.filterwarnings('ignore')
+# warnings.filterwarnings('ignore')
 
 class Model:
     def __init__(self):
 
         # normalisasi
         normalisasi = joblib.load("H:\\3_hibrid_learning\\3_hibrid_learning_kali\\new_benign\\normalisasi\\normalisasi_minmaxScaler_rasio_4_1.joblib")
-        #normalisasi = joblib.load("H:\\3_hibrid_learning\\normalisasi_minmaxScaler_rasio_4_1.joblib")
 
         # fitur yang dihapus
         fitur_dihapus_1 = pd.read_csv("H:\\3_hibrid_learning\\3_hibrid_learning_kali\\rasio_4_1\\fitur_hapus\\fitur_dihapus_layer_1_rasio_4_1.txt")
         fitur_dihapus_2 = pd.read_csv("H:\\3_hibrid_learning\\3_hibrid_learning_ori\\rasio_4_1\\fitur_hapus\\fitur_dihapus_layer_2_rasio_4_1.txt")
         fitur_dihapus_3 = pd.read_csv("H:\\3_hibrid_learning\\3_hibrid_learning_ori\\rasio_4_1\\fitur_hapus\\fitur_dihapus_layer_3_rasio_4_1.txt")
-        #tambah_fitur = ['dst_port', 'protocol', 'timestamp']
-        #fitur_dihapus_1 = [*fitur_dihapus_1['0'].values.tolist(), *tambah_fitur]
-        #fitur_dihapus_2 = [*fitur_dihapus_2['0'].values.tolist(), *tambah_fitur]
-        #fitur_dihapus_3 = [*fitur_dihapus_3['0'].values.tolist(), *tambah_fitur]
 
         # kolom dataset
         X_kolom = pd.read_csv("H:\\3_hibrid_learning\\3_hibrid_learning_ori\\rasio_4_1\\fitur\\fitur_rasio_4_1.txt")
-        #X_kolom = pd.read_csv("H:\\3_hibrid_learning\\fitur_rasio_4_1.txt")
 
         print('')
         print("".center(34, '='))
@@ -38,19 +32,16 @@ class Model:
         # lapisan 1 - lstm
         print (" Memuat model lstm pada lapisan 1 ")
         model_lapisan_1 = keras.models.load_model("H:\\3_hibrid_learning\\3_hibrid_learning_kali\\new_benign\\model\\model_lstm_layer_1_rasio_4_1.joblib")
-        #model_lapisan_1 = keras.models.load_model("H:\\3_hibrid_learning\\model_lstm_layer_1.joblib")
         print (" Selesai memuat ")
     
         # lapisan 2 - random forest
         print (" Memuat model random forest pada lapisan 2 ")
         model_lapisan_2 = joblib.load("H:\\3_hibrid_learning\\3_hibrid_learning_kali\\new_benign\\model\\model_rf_layer_2_rasio_4_1.joblib")
-        #model_lapisan_2 = joblib.load("H:\\3_hibrid_learning\\model_rf_layer_2.joblib")
         print (" Selesai memuat ")
         
         # lapisan 3 multi - random forest
         print (" Memuat model random forest pada lapisan 3 ")
         model_lapisan_3 = joblib.load("H:\\3_hibrid_learning\\3_hibrid_learning_kali\\new_benign\\model\\model_rf_layer_3_rasio_4_1.joblib")
-        #model_lapisan_3 = joblib.load("H:\\3_hibrid_learning\\model_rf_layer_3.joblib")
         print (" Selesai memuat ")
         
         # membuat objek 
